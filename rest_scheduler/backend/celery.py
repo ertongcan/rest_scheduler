958cf65b792b68ca9d643f0849c7c7ba894d3d6b
Can Ertong <can.ertong@orionfintechinc.com>
2021-04-15 01:47:14 +0300

    celery support

--- rest_scheduler/backend/celery.py
+++ rest_scheduler/backend/celery.py
@@ -1,25 +1,13 @@
-from __future__ import absolute_import, unicode_literals
+import celery import Celery
 
 import os
 
-from celery import Celery
 
-# set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+from django.conf import settings
 
 app = Celery('backend')
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
-# Using a string here means the worker doesn't have to serialize
-# the configuration object to child processes.
-# - namespace='CELERY' means all celery-related configuration keys
-#   should have a `CELERY_` prefix.
-app.config_from_object('django.conf:settings', namespace='CELERY')
-
-# Load task modules from all registered Django app configs.
-app.autodiscover_tasks()
-
-
-@app.task(bind=True)
-def test_task(self):
-    print('Request: {0!r}'.format(self.request))
-    return 42
+app.conf.update(BROKER_URL=os.environ['REDIS_URL'],
+                CELERY_RESULT_BACKEND=os.environ['REDIS_URL'])
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
--- rest_scheduler/backend/tasks.py
+++ rest_scheduler/backend/tasks.py
@@ -1,14 +1,6 @@
-import celery
-app = celery.Celery('backend')
-import os
+from backend.celery import app
 
-from django.conf import settings
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
-
-app.conf.update(BROKER_URL=os.environ['REDIS_URL'],
-                CELERY_RESULT_BACKEND=os.environ['REDIS_URL'])
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 @app.task
-def add():
-    return "Hello"
+def test():
+    print("Hello")
