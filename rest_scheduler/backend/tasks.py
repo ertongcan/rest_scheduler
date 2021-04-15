ff8887e8bb32e7e55866178fbc1a7d4e30986164
Can Ertong <can.ertong@orionfintechinc.com>
2021-04-15 22:01:08 +0300

    new task

--- rest_scheduler/backend/tasks.py
+++ rest_scheduler/backend/tasks.py
@@ -2,9 +2,9 @@ from celery import shared_task
 import requests
 
 @shared_task
-def process_request(url, verb, headers = None, params = None, timeout = 5):
+def process_request(url, method, **kwargs):
 	#task_arguments = kwargs
 	
-	r = requests.request(verb, url, headers= headers, params = params, timeout=timeout)
-	r.encoding = 'ISO-8859-1'
-	return r.text
+	#r = requests.request(method, url, headers= headers, params = params, timeout=timeout)
+	#r.encoding = 'ISO-8859-1'
+	return url, method, kwargs
