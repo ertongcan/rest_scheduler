8b558154a384603f3618bd35b584020815083068
Can Ertong <can.ertong@orionfintechinc.com>
2021-04-15 23:17:36 +0300

    new task

--- rest_scheduler/backend/tasks.py
+++ rest_scheduler/backend/tasks.py
@@ -4,13 +4,20 @@ import requests
 @shared_task
 def process_request(url, method, **kwargs):
 
+	result = None
 	try:
 		resp = requests.request(method, url)
+		r.raise_for_status()
+		result = resp.json()
+	except requests.exceptions.RequestException as err:
+		result = f"OOps: Something Else {err}"
+	except requests.exceptions.HTTPError as errh:
+		result = f"Http Error: {errh}"
+	except requests.exceptions.ConnectionError as errc:
+		result = f"Error Connecting: {errc}"
+	except requests.exceptions.Timeout as errt:
+		result = f"Timeout Error: {errt}" 
 		
-		if not resp.ok:
-			resp.raise_for_status()
-		else:
-			return resp.json()
-	except Exception as e:
-		resp.raise_for_status()
+	
+	return result
 	
