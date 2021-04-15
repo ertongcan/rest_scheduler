767c610eaaff5e01fea128ac198c8be81e280f9c
Can Ertong <can.ertong@orionfintechinc.com>
2021-04-15 21:57:23 +0300

    new task

--- rest_scheduler/backend/tasks.py
+++ rest_scheduler/backend/tasks.py
@@ -6,5 +6,5 @@ def process_request(url, verb, headers = None, params = None, timeout = 5):
 	#task_arguments = kwargs
 	
 	r = requests.request(verb, url, headers= headers, params = params, timeout=timeout)
-	
+	r.encoding = 'ISO-8859-1'
 	return r.text
