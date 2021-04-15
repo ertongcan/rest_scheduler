36585a40688a130822fbbe9cf270a8be89abf368
Can Ertong <can.ertong@orionfintechinc.com>
2021-04-15 03:41:28 +0300

    celery support

--- rest_scheduler/backend/celery.py
+++ rest_scheduler/backend/celery.py
@@ -8,9 +8,8 @@ from django.conf import settings
 app = Celery('backend')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
-# app.conf.update(BROKER_URL=os.environ['REDIS_URL'],
-  #              CELERY_RESULT_BACKEND='django-db',BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler')
-app.conf.update()
+app.conf.update(BROKER_URL=os.environ['REDIS_URL'], CELERY_RESULT_BACKEND='django-db',BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler')
+
 app.config_from_object('django.conf:settings', namespace='CELERY')
 @app.task(bind=True)
 def debug_task(self):
