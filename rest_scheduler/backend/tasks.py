1901ad4c9b3cbaffd704883216300a0787428b75
Can Ertong <can.ertong@orionfintechinc.com>
2021-04-15 21:44:22 +0300

    new task

--- rest_scheduler/backend/tasks.py
+++ rest_scheduler/backend/tasks.py
@@ -1,11 +1,10 @@
 from celery import shared_task
+import requests
 
 @shared_task
-def process_request(url):
+def process_request(url, verb, headers = None, params = None, timeout = 5):
 	#task_arguments = kwargs
 	
-	#r = requests.get(
-     #           f'https://api.btohq.com/api/getSalesLaunchTableData?draw=7&columns%5B0%5D%5Bdata%5D=Project&columns%5B0%5D%5Bname%5D=&columns%5B0%5D%5Bsearchable%5D=true&columns%5B0%5D%5Borderable%5D=true&columns%5B0%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B0%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B1%5D%5Bdata%5D=Flat_Type&columns%5B1%5D%5Bname%5D=&columns%5B1%5D%5Bsearchable%5D=true&columns%5B1%5D%5Borderable%5D=true&columns%5B1%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B1%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B2%5D%5Bdata%5D=Block_No&columns%5B2%5D%5Bname%5D=&columns%5B2%5D%5Bsearchable%5D=true&columns%5B2%5D%5Borderable%5D=true&columns%5B2%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B2%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B3%5D%5Bdata%5D=Unit_No&columns%5B3%5D%5Bname%5D=&columns%5B3%5D%5Bsearchable%5D=true&columns%5B3%5D%5Borderable%5D=true&columns%5B3%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B3%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B4%5D%5Bdata%5D=Size&columns%5B4%5D%5Bname%5D=&columns%5B4%5D%5Bsearchable%5D=true&columns%5B4%5D%5Borderable%5D=true&columns%5B4%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B4%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B5%5D%5Bdata%5D=Price&columns%5B5%5D%5Bname%5D=&columns%5B5%5D%5Bsearchable%5D=true&columns%5B5%5D%5Borderable%5D=true&columns%5B5%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B5%5D%5Bsearch%5D%5Bregex%5D=false&order%5B0%5D%5Bcolumn%5D=0&order%5B0%5D%5Bdir%5D=asc&start={st}&length=10&search%5Bvalue%5D=&search%5Bregex%5D=false&salesLaunchId={sli}&_=1616708223968',
-      #          headers=_headers(), proxies=PROXIES, timeout=10)
+	r = requests.request(verb, headers, timeout=timeout)
 	
-	return url
+	return r.text
