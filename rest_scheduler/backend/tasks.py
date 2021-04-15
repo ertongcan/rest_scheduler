6ff3550db3e6e2a700ebac95c472b62d72a28945
Can Ertong <can.ertong@orionfintechinc.com>
2021-04-15 23:27:07 +0300

    new task

--- rest_scheduler/backend/tasks.py
+++ rest_scheduler/backend/tasks.py
@@ -10,14 +10,12 @@ def process_request(url, method, **kwargs):
 		resp.raise_for_status()
 		result = resp.json()
 	except requests.exceptions.RequestException as err:
-		result = f"OOps: Something Else {err}"
+		raise(f"Request Error: {err}")
 	except requests.exceptions.HTTPError as errh:
-		result = f"Http Error: {errh}"
+		raise(f"Http Error: {errh}")
 	except requests.exceptions.ConnectionError as errc:
-		result = f"Error Connecting: {errc}"
+		raise(f"Error Connecting: {errc}")
 	except requests.exceptions.Timeout as errt:
-		result = f"Timeout Error: {errt}" 
+		raise(f"Timeout Error: {errt}") 
 		
-	
 	return result
-	
